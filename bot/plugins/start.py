0923cea85f28f08b41051df867298da18c9e1077
BOBworker <45184121+AndersonCoin@users.noreply.github.com>
2025-10-22 11:56:47 +0100

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from bot.helpers.localization import get_text, set_language
+from bot.helpers.keyboards import get_language_keyboard
 
+@Client.on_message(filters.command(["start", "help"]))
+async def start_command(client: Client, message: Message):
+    user_id = message.from_user.id
+    await message.reply_text(
+        get_text(user_id, "help_text"),
+        reply_markup=get_language_keyboard()
+    )
+
+@Client.on_message(filters.command("language"))
+async def language_command(client: Client, message: Message):
+    await message.reply_text(
+        "Please choose a language:",
+        reply_markup=get_language_keyboard()
+    )
