82b60356adff3d965617146f061bfb04384aa5e6
BOBworker <45184121+AndersonCoin@users.noreply.github.com>
2025-10-22 17:08:06 +0100

    Update queue.py

--- bot/plugins/queue.py
+++ bot/plugins/queue.py
@@ -2,33 +2,39 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
+
 from bot.core.queue import queue_manager
+from bot.helpers.formatting import formatting
+from bot.helpers.keyboards import keyboards
 from bot.helpers.localization import get_text
-from bot.helpers.formatting import format_queue_list
-from bot.helpers.keyboards import Keyboards
+
 
 @Client.on_message(filters.command("queue") & filters.group)
 async def queue_command(client: Client, message: Message):
     """Handle /queue command."""
     chat_id = message.chat.id
-    user_id = message.from_user.id
-    
-    # Get queue
     queue = queue_manager.get_queue(chat_id)
     
-    if not queue.tracks:
-        await message.reply(get_text(user_id, "queue_empty"))
+    if queue.is_empty:
+        await message.reply_text(get_text(message.chat, "queue_empty"))
         return
     
-    # Format queue
-    text, total_pages = format_queue_list(queue.tracks, page=1)
+    # Get first page
+    tracks, total_pages = queue.get_page(1, 10)
     
-    if not text:
-        await message.reply(get_text(user_id, "queue_empty"))
-        return
+    # Build queue text
+    text = get_text(message.chat, "queue_title", page=1, total_pages=total_pages) + "\n\n"
     
-    # Send queue message
-    await message.reply(
-        get_text(user_id, "queue_title", page=1, total_pages=total_pages) + "\n\n" + text,
-        reply_markup=Keyboards.queue_navigation(chat_id, 1, total_pages)
+    for i, track in enumerate(tracks, 1):
+        text += get_text(
+            message.chat,
+            "queue_item",
+            index=i,
+            title=track.title,
+            duration=formatting.duration(track.duration)
+        ) + "\n"
+    
+    await message.reply_text(
+        text,
+        reply_markup=keyboards.queue_navigation(chat_id, 1, total_pages)
     )
