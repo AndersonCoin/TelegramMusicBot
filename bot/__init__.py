e542150f82a2cf579ad343943066a532f366e3e4
BOBworker <45184121+AndersonCoin@users.noreply.github.com>
2025-10-22 14:10:10 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,1 @@
-"""
-Main bot package.
 
-This file makes the 'bot' directory a Python package and can be used
-to expose key components from sub-modules for easier access.
-"""
-
-from .client import app
-
-# This makes `from bot import app` possible elsewhere in the project.
-__all__ = ["app"]
