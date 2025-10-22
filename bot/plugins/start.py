7f2f29b663f9d696d09c78060069bf2d1750442c
BOBworker <45184121+AndersonCoin@users.noreply.github.com>
2025-10-22 16:10:05 +0100

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,19 +1,24 @@
+"""Start and help commands."""
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from bot.helpers.localization import get_text, set_language
-from bot.helpers.keyboards import get_language_keyboard
+from bot.helpers.localization import get_text, localization
+from bot.helpers.keyboards import Keyboards
 
-@Client.on_message(filters.command(["start", "help"]))
+@Client.on_message(filters.command(["start", "help"]) & filters.private)
 async def start_command(client: Client, message: Message):
+    """Handle /start and /help commands."""
     user_id = message.from_user.id
-    await message.reply_text(
-        get_text(user_id, "help_text"),
-        reply_markup=get_language_keyboard()
+    
+    await message.reply(
+        get_text(user_id, "start_message"),
+        reply_markup=Keyboards.start_buttons(user_id)
     )
 
 @Client.on_message(filters.command("language"))
 async def language_command(client: Client, message: Message):
-    await message.reply_text(
-        "Please choose a language:",
-        reply_markup=get_language_keyboard()
+    """Handle /language command."""
+    await message.reply(
+        get_text(message.from_user.id, "choose_language"),
+        reply_markup=Keyboards.language_buttons()
     )
