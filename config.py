e9ff5d5fc583d39aebd04e8f51e2a87c18707169
BOBworker <45184121+AndersonCoin@users.noreply.github.com>
2025-11-06 12:46:14 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 class Config:
     # Telegram API credentials
-    API_ID = 28737273
-    API_HASH = "b2f71f8c2cb0c911a85dee75220bb348"
-    BOT_TOKEN = "7951060445:AAGi4k4xdKEKCjxTBggAuBKiNJp_lxHfkxc"
-    SESSIN_STRING = "BAG2fvkAB6xeQkFhRwOX3xu6Ol3iOaWJAkiHkAV26txYOTq-HMELAHlj4nVfhvGScyWigCxDP2Q2M_pQfLoi1lW63Tib_rGgTs1VRki9hhnPAODM5QjmL2VgDBRrdooW1Nfl73Ahz4oD5QaMRWjBFy_9RDxaZ45DQzgMrl24e7H8n2k1D9uAbsxX4nlPrQnT0n0esPiSDb9g_Rbg3akx5kYBn1wnbc2VL-Iqd7HQ3S0c_wKDaRhOSjEgdg82Zk34WbXXQOl2pb2oeDLCokepvJIVNsUpdMgGBR9_UILbfmv-NNSvQGe9xmrh1nAXNDEm6PPyO7F8OzfuLfuOKowrq3UArvCgNQAAAAHOEwb_AA"
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    
     # Bot settings
-    OWNER_ID = "178432304"
+    OWNER_ID = int(os.getenv("OWNER_ID", 0))
     SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "")
     
     # Music settings
